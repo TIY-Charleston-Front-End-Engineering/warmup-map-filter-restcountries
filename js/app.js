eb1704a7da84ecc8ab4fca20be10c8462a0ab587
Travis Hubbard <t3patterson@gmail.com>
2016-10-24 09:15:01 -0400

    adding prompt

--- js/app.js
+++ js/app.js
@@ -1,4 +1,32 @@
-console.log('wired up!')
-console.log($)
-console.log(_)
-console.log(Backbone)
+//-----------------------------------------
+// .map()
+//   Use the  Array.prototype.map() function to return an array of
+//   strings as names for all the countries
+//------------------------------------------
+
+$.getJSON("https://restcountries.eu/rest/v1/all").then(function(resultsArray){
+   // YOUR WORK HERE
+   // resultsArray.map()
+})
+
+
+//-----------------------------------------
+// .filter()
+//   Use the Array.prototype.filter() function to return an array of
+//   JSON objects that are in the Asia region
+//------------------------------------------
+
+
+//-----------------------------------------
+// .filter().map()
+//   Chain the Array.prototype.filter() and .map() functions  to return an array of
+//   strings with the names of countries that have populations  < 5,000,000
+//------------------------------------------
+
+
+// BONUS!
+//-----------------------------------------
+// .reduce()
+//   Use the Array.prototype.reduce() function to calculate the total population
+//   number value for those in countries with total populations < 5,000,000
+//------------------------------------------
